2af3e615a15b9a886b233dc2e5c23b0efe6c471a
Eric Damtoft <edamtoft@gmail.com>
2021-11-14 13:45:51 -0500

    Add benchmarks

---
+++ source/TechTalks.Generators.Benchmarks/Json/JsonBenchmarks.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using System.Threading.Tasks;
+
+namespace TechTalks.Generators.Benchmarks.Json
+{
+  public class JsonBenchmarks
+  {
+    [Benchmark]
+    public void WithReflection()
+    {
+      // New options to prevent caching between runs
+
+      var options = new JsonSerializerOptions();
+
+      // Round-trip serialize
+
+      var json = JsonSerializer.Serialize(new Sample
+      {
+        Property1 = "Hello World",
+        Property2 = false,
+        Property3 = Guid.NewGuid(),
+        Property4 = 123
+      }, options);
+
+      _ = JsonSerializer.Deserialize<Sample>(json, options);
+    }
+
+    [Benchmark]
+    public void WithSourceGen()
+    {
+      // New context to prevent caching between runs
+
+      var context = new SampleContext(new JsonSerializerOptions());
+
+      // Round-trip serialize
+
+      var json = JsonSerializer.Serialize(new Sample
+      {
+        Property1 = "Hello World",
+        Property2 = false,
+        Property3 = Guid.NewGuid(),
+        Property4 = 123
+      }, context.Sample);
+
+      _ = JsonSerializer.Deserialize(json, context.Sample);
+    }
+  }
+}
---
+++ source/TechTalks.Generators.Benchmarks/Json/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTalks.Generators.Benchmarks.Json
+{
+  internal class Sample
+  {
+    public string? Property1 { get; set; }
+    public bool Property2 { get; set; }
+    public Guid Property3 { get; set; }
+    public int Property4 { get; set; }
+  }
+}
